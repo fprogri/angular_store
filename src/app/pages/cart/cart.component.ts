bda5352f0f4f11f0ac5c9408b7d58f860dc9ead2
Florian Progri <florian.progri@ikubinfo.al>
2022-12-22 14:48:07 +0100

    a

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,12 +1,35 @@
 import { Component, OnInit } from "@angular/core";
+import { Cart, CartItem } from "src/app/models/cart.model";
 
 @Component({
   selector: "app-cart",
   templateUrl: "./cart.component.html",
-  styles: [],
 })
 export class CartComponent implements OnInit {
-  constructor() {}
+  cart: Cart = {
+    items: [
+      {
+        image:
+          "https://cdn-prod.medicalnewstoday.com/content/images/articles/271/271157/bananas-chopped-up-in-a-bowl.jpg",
+        name: "Banana",
+        price: 3,
+        quantity: 5,
+        id: 9,
+      },
+    ],
+  };
 
-  ngOnInit(): void {}
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    "image",
+    "name",
+    "price",
+    "quantity",
+    "total",
+    "action",
+  ];
+  constructor() {}
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
 }
